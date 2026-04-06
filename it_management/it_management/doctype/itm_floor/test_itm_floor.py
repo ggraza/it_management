5676f959b8e84c0f679c25efdf328ffb13fd98ae
trustedcomputer <github@trustedcomputer.pro>
2026-04-06 14:29:56 -0700

    refactor: add itm prefix to location-related doctype fieldnames

---
+++ it_management/it_management/doctype/itm_floor/test_itm_floor.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2026, IT-Geräte und IT-Lösungen wie Server, Rechner, Netzwerke und E-Mailserver sowie auch Backups, and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase
+
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record dependencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+
+class IntegrationTestITMFloor(IntegrationTestCase):
+	"""
+	Integration tests for ITMFloor.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
